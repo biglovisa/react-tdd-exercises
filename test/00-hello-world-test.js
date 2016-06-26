28c283b8f6413b1f173bd202c2eac669550089a3
Lovisa Svallingson <lovisasvallingson@gmail.com>
2016-06-26 16:11:04 -0600

    initial commit: HelloWorld

---
+++ test/00-hello-world-test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import HelloWorld from '../src/00/HelloWorld';
+
+describe("Component: Hello World", function() {
+
+  it("contains a div with class `hello-world`", function() {
+    expect(shallow(<HelloWorld />).find("div.hello-world")).to.have.length(1);
+  });
+
+  it("contains an h1 tag", function() {
+    expect(shallow(<HelloWorld />).find('h1')).to.have.length(1);
+  });
+
+  it("contains the text `Hello, World!`", function() {
+    expect(shallow(<HelloWorld />).text()).to.equal('Hello, World!');
+  });
+});
