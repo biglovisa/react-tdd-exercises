b159cbd6a1da4475fa1cabea36d575c0b33f2170
Lovisa Svallingson <lovisasvallingson@gmail.com>
2016-06-26 19:03:09 -0600

    Skip tests

--- README.md
+++ README.md
@@ -12,6 +12,13 @@ Testing tools: [Enzyme](http://airbnb.io/enzyme/), [Mocha](https://mochajs.org/)
 
 In the test folder there are enumerated test suites and every test in every test suite is skipped. Work with one test suite at a time, starting with the first one. Unskip one test at a time and write code to make it pass.
 
+If you want to debug your components from the browser, run `webpack` in your terminal and then open `index.html`.
+
+```sh
+$ webpack
+$ open index.html
+```
+
 ## Up and running
 
 Clone the repository and install the dependencies:
--- test/00-hello-world-test.js
+++ test/00-hello-world-test.js
@@ -5,15 +5,15 @@ import HelloWorld from '../src/00/HelloWorld';
 
 describe("Component: Hello World", function() {
 
-  it("contains a div with class `hello-world`", function() {
+  xit("contains a div with class `hello-world`", function() {
     expect(shallow(<HelloWorld />).find("div.hello-world")).to.have.length(1);
   });
 
-  it("contains an h1 tag", function() {
+  xit("contains an h1 tag", function() {
     expect(shallow(<HelloWorld />).find('h1')).to.have.length(1);
   });
 
-  it("contains the text `Hello, World!`", function() {
+  xit("contains the text `Hello, World!`", function() {
     expect(shallow(<HelloWorld />).text()).to.equal('Hello, World!');
   });
 });
