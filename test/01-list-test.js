d510901b4d71fa0dbfd12dc501a3109c56d13adc
Lovisa Svallingson <lovisasvallingson@gmail.com>
2016-06-29 21:44:49 -0600

    Add list tests

--- test/01-list-test.js
+++ test/01-list-test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow, mount } from 'enzyme';
-import List from '../src/01/List';
+import React from "react";
+import { expect } from "chai";
+import { shallow, mount } from "enzyme";
+import List from "../src/01/ListTwo";
 
 describe.only("Component: List", function() {
 
-  xit("contains a div with class `list`", function() {
-    expect(shallow(<List />).find("div.list")).to.have.length(1);
+  xit("contains a div with class `content`", function() {
+    expect(shallow(<List />).find("div.content")).to.have.length(1);
   });
 
   xit("has an input field", function() {
@@ -25,24 +25,54 @@ describe.only("Component: List", function() {
     expect(wrapper.state().names.length).to.equal(0);
   });
 
-  xit("has a state `current` which is initialized as an empty string", function() {
-    const wrapper = shallow(<List />);
-    expect(wrapper.state().current).to.equal("");
+  xit("renders an h1 if the state is empty", function() {
+    const wrapper = mount(<List />);
+
+    expect(shallow(<List />).find("div.content")).to.have.length(1);
+    expect(wrapper.find("div.content")).to.have.length(1);
+    expect(wrapper.find("div.content").text()).to.include("Nothing here yet :(");
   });
 
-  xit("the input field has an onChange event listener", function() {
-    const inputField = shallow(<List />).find("input");
-    expect(inputField.props().onChange).to.be.instanceOf(Function);
-  });
-
-  xit("submitting text in the input field adds to the state", function() {
+  xit("submitting text in the input field adds to state", function() {
     const wrapper = mount(<List />);
     const name = "Mariko";
-    wrapper.setState({ current: name })
+    wrapper.setState({ current: name });
 
     wrapper.find("button").simulate("click");
 
     expect(wrapper.state().names.length).to.equal(1);
     expect(wrapper.state().names[0]).to.equal(name);
+  });
+
+  xit("renders submitted text", function() {
+    const wrapper = mount(<List />);
+    const name = "Mariko";
+    wrapper.setState({ names: [name] })
+
+    expect(wrapper.find("div.content")).to.have.length(1);
+    expect(wrapper.find("div.content")).to.have.length(1);
+
+    expect(wrapper.find("div.content").text()).to.not.include("Nothing here yet :(");
+    expect(wrapper.state().names.length).to.equal(1);
+    expect(wrapper.find("div.content").text()).to.include(name);
+  });
+
+  xit("renders all submitted text", function() {
+    const wrapper = mount(<List />);
+    let name = "Mariko";
+    wrapper.setState({ names: [name] })
+
+    expect(shallow(<List />).find("div.content")).to.have.length(1);
+    expect(wrapper.find("div.content")).to.have.length(1);
+
+    expect(wrapper.find("div.content").text()).to.not.include("Nothing here yet :(");
+    expect(wrapper.state().names.length).to.equal(1);
+    expect(wrapper.find("div.content").text()).to.include(name);
+
+    let name2 = "Ted";
+    wrapper.setState({ names: [name, name2] });
+
+    expect(wrapper.find("div.content").text()).to.include(name);
+    expect(wrapper.find("div.content").text()).to.include(name2);
   });
 });
