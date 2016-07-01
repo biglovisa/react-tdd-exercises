12004a1d2e3876254a7626e9764a524a739927ad
Will Faurot <will@influxdb.com>
2016-07-01 00:17:01 -0700

    Add skeleton for Dropdown implementation

---
+++ src/02/Dropdown.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class Dropdown extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className='dropdown'>
+      </div>
+    );
+  }
+}
+
+Dropdown.propTypes = {
+};
+
+export default Dropdown;
