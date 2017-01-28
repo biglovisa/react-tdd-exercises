f332dddc0ffc57216cdb07e2d73fad9e62e9f475
PlanetEfficacy <jspevack@gmail.com>
2017-01-28 14:19:12 -0700

    Add ToggleButton component skeleton

---
+++ src/04/ToggleButton.js
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+class ToggleButton extends Component {
+  constructor() {
+    super();
+  }
+
+  render () {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+ToggleButton.PropTypes = {
+};
+
+export default ToggleButton;
